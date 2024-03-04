259b321ea06efbc2c0182550c4cd879fbe8c7e55
Nikola Stankovic <jonnywintech@gmail.com>
2024-03-04 20:59:57 +0100

    forgot to add middleware aliases

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,12 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
 
         $this->call(AdminSeeder::class);
         $this->call(RoleSeeder::class);
